08145f24dcffe7940fd2b0302e76037c1acb6373
elegant1225 <1737289219@qq.com>
2016-10-06 21:03:50 +0800

    配置环境，连接数据库

---
+++ application/controllers/blog.php
@@ -0,0 +1,11 @@
+<?php
+    defined('BASEPATH')OR exit('No direct script access allowed');
+        class Blog extends CI_Controller{
+            public function __construct(){
+                parent::__construct();
+            }
+            public function index(){
+               // echo 222;默认情况下的函数输出内容
+            }
+        }
+?>
